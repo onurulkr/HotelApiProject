3a1a124a85b75b6eae825da9de3b299ba8778818
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-05-11 17:21:44 +0300

    Update Startup.cs

--- ApiConsume/HotelProject.WebApi/Startup.cs
+++ ApiConsume/HotelProject.WebApi/Startup.cs
@@ -57,7 +57,7 @@ namespace HotelProject.WebApi
             });
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(c => 
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelProject.WebApi", Version = "v1" });
             });

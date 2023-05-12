882d2df0492a4982edbb9350ee7eacfbcd58d412
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-05-12 17:45:36 +0300

    Update AutoMapperConfig.cs

--- Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -4,7 +4,7 @@
 //using System;
 //using System.Collections.Generic;
 //using System.Linq;
-//using System.Threading.Tasks;
+//using System.Threading.Tasks; 
 
 //namespace HotelProject.WebUI.Mapping
 //{

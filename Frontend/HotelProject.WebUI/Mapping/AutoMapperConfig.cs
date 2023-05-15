0c46f3556663a8c1058911587f04dc158225eae6
onur.ulker <onur.ulker@WRPNW063>
2023-05-15 16:32:13 +0300

    updated

--- Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -1,21 +1,21 @@
-﻿//using AutoMapper;
-//using HotelProject.DtoLayer.Dtos.RoomDto;
-//using HotelProject.EntityLayer.Concrete;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using AutoMapper;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace HotelProject.WebUI.Mapping
-//{
-//    public class AutoMapperConfig : Profile
-//    {
-//        public AutoMapperConfig()
-//        {
-//            CreateMap<RoomAddDto, Room>();
-//            CreateMap<Room, RoomAddDto>();
+namespace HotelProject.WebUI.Mapping
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<RoomAddDto, Room>();
+            CreateMap<Room, RoomAddDto>();
 
-//            CreateMap<RoomUpdateDto, Room>().ReverseMap();
-//        }
-//    }
-//}
+            CreateMap<RoomUpdateDto, Room>().ReverseMap();
+        }
+    }
+}

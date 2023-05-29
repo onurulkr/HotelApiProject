4d629783050f5765f7cc8481925dfe2b77accc90
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-05-29 17:19:54 +0300

    Update ImdbController.cs

--- RapidApi/RapidApiConsume/Controllers/ImdbController.cs
+++ RapidApi/RapidApiConsume/Controllers/ImdbController.cs
@@ -11,7 +11,7 @@ namespace RapidApiConsume.Controllers
     {
         public async Task<IActionResult> Index()
         {
-            var client = new HttpClient();
+            var client = new HttpClient(); 
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,

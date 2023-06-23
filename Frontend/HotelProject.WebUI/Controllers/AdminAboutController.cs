50826ab6f539da3ee4aab7c9de3cc1be1e7bf775
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-06-23 17:07:02 +0300

    Update AdminAboutController.cs

--- Frontend/HotelProject.WebUI/Controllers/AdminAboutController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminAboutController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic; 
 using System.Linq;
 using System.Net.Http;
 using System.Text;

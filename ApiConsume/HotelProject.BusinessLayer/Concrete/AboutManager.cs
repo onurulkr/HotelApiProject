34c525beba2ef662ede524b9b1d5f51a2df86e7e
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-07-07 15:31:57 +0300

    Update AboutManager.cs

--- ApiConsume/HotelProject.BusinessLayer/Concrete/AboutManager.cs
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/AboutManager.cs
@@ -4,7 +4,7 @@ using HotelProject.EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks; 
 
 namespace HotelProject.BusinessLayer.Concrete

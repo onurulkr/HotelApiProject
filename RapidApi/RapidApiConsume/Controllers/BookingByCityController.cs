75fc61ff884a4b410049b3fcee64518d8c55ed38
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-07-25 17:49:25 +0300

    Update BookingByCityController.cs

--- RapidApi/RapidApiConsume/Controllers/BookingByCityController.cs
+++ RapidApi/RapidApiConsume/Controllers/BookingByCityController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq; 
 using System.Net.Http;
 using System.Threading.Tasks;
-
+ 
 namespace RapidApiConsume.Controllers
 {
     public class BookingByCityController : Controller 

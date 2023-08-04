f97355501854dea17df2c5cd6f332afc1358ad20
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-08-04 11:03:41 +0300

    Update IBookingService.cs

--- ApiConsume/HotelProject.BusinessLayer/Abstract/IBookingService.cs
+++ ApiConsume/HotelProject.BusinessLayer/Abstract/IBookingService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace HotelProject.BusinessLayer.Abstract
 {
     public interface IBookingService : IGenericService<Booking>
-    {
+    { 
         void TBookingStatusChangeApproved(Booking booking);
         void TBookingStatusChangeApproved2(int id);
     }

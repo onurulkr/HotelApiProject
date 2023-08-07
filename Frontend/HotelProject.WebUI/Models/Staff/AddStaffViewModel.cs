b137216e62a7da609c6b7cba415f2aa7d4ad7bcd
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-08-07 09:50:08 +0300

    Update AddStaffViewModel.cs

--- Frontend/HotelProject.WebUI/Models/Staff/AddStaffViewModel.cs
+++ Frontend/HotelProject.WebUI/Models/Staff/AddStaffViewModel.cs
@@ -10,7 +10,7 @@ namespace HotelProject.WebUI.Models.Staff
         public int StaffId { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
-        public string SocialMedia1 { get; set; } 
+        public string SocialMedia1 { get; set; }  
         public string SocialMedia2 { get; set; }
         public string SocialMedia3 { get; set; }
     }

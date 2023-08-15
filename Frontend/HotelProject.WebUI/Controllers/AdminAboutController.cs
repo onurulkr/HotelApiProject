50c8ae154bdc32efb446081bd0813c22f7b15138
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-08-15 17:30:26 +0300

    Update AdminAboutController.cs

--- Frontend/HotelProject.WebUI/Controllers/AdminAboutController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminAboutController.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic; 
 using System.Linq;
 using System.Net.Http;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks; 
 
 namespace HotelProject.WebUI.Controllers 

72bf45226c412acfdb578e6647f1e912b3fca13a
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-08-25 17:47:08 +0300

    Update IAboutService.cs

--- ApiConsume/HotelProject.BusinessLayer/Abstract/IAboutService.cs
+++ ApiConsume/HotelProject.BusinessLayer/Abstract/IAboutService.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelProject.BusinessLayer.Abstract  
-{
+{ 
     public interface IAboutService : IGenericService<About>     
     { 
     }

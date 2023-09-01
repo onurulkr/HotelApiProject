bbf6e7b1b7c886c3cd935d2679d18bb62f963b1d
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-09-01 15:20:00 +0300

    Update BookingAdminController.cs

--- Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
+++ Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; 
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;

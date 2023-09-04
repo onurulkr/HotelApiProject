35b3cfaadc81dbecceadbb9e581b8ffbf6045859
onurulkr <55154950+onurulkr@users.noreply.github.com>
2023-09-04 16:14:50 +0300

    Update CreateToken.cs

--- JwtProject/WebApiJwt/Models/CreateToken.cs
+++ JwtProject/WebApiJwt/Models/CreateToken.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks; 
 
 namespace WebApiJwt.Models
